b689067ff28c4c47df04c65851451885cf4510fe
CharlieOConnor <charrlie.oconnor98@gmail.com>
2018-11-23 13:47:22 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+package eightQueens;
+
+public class Main {
+
+	public static void main(String args[]) {
+
+		Queens queens = new Queens();
+		System.out.println(queens);
+	}
+}
---
+++ Queens.java
@@ -0,0 +1,39 @@
+package eightQueens;
+
+import java.util.Scanner;
+
+public class Queens {
+
+	  int[][] chessBoard = new int[8][8];
+
+	public Queens() {
+		
+		int numPosition = 8; //Placeholder for number positions on the chess board
+
+		System.out.print("This is an 8 Queens chessboard\n\n");
+	    System.out.print("Please enter a position on the board (i.e. D3)\n");
+	    
+	    //placeQueen();
+	    
+	    System.out.print("\n    a   b   c   d   e   f   g   h");
+
+		for (int row = 0; row < chessBoard.length; row++) //A nested for loop to create the chess board												
+		{
+			System.out.print("\n  +-------------------------------+\n" + numPosition-- + " |");
+
+			for (int column = 0; column < chessBoard[row].length; column++) {
+				System.out.print("   |");
+			}
+		}
+		System.out.print("\n  +-------------------------------+\n");
+	}
+	
+	public void placeQueen() //Enter queen at user's specified position
+	{
+     Scanner scan = new Scanner(System.in);
+     String boardPosition = scan.next();
+     
+     
+     scan.close();
+	}
+}
