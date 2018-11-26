09ec2cff9691441becf58077b596106a4a39195b
CharlieOConnor <charrlie.oconnor98@gmail.com>
2018-11-26 11:30:20 +0000

    Update Queens.java

--- Queens.java
+++ Queens.java
@@ -4,13 +4,20 @@ import java.util.Scanner;
 
 public class Queens {
 
-	int[][] chessBoard = new int[8][8];
+	String[][] chessBoard = new String[8][8];
+
+	/** Maybe change to a string? (Originally an int) */
 
 	public Queens() {
 
 		int numPosition = 8; // Placeholder for number positions on the chess
 								// board
-
+		for (int i = 0; i < chessBoard.length; i++) {
+			for (int j = 0; j < chessBoard[i].length; j++) {
+				chessBoard[i][j] = " ";
+			}
+		}
+		
 		System.out.print("This is an 8 Queens chessboard\n\n");
 		System.out.print("Please enter a position on the board (i.e. D3)\n");
 
@@ -18,43 +25,46 @@ public class Queens {
 
 		System.out.print("\n    a   b   c   d   e   f   g   h");
 
-		for (int row = 0; row < chessBoard.length; row++) // A nested for loop to create the chess board
+		for (int row = 0; row < chessBoard.length; row++) // A nested for loop
+															// to create the
+															// chess board
 		{
 			System.out.print("\n  +-------------------------------+\n" + numPosition-- + " |");
 
 			for (int column = 0; column < chessBoard[row].length; column++) {
-				System.out.print("   |");
+				System.out.print(" ");
+				System.out.print(chessBoard[row][column]);
+				System.out.print(" |");
 			}
 		}
 		System.out.print("\n  +-------------------------------+\n");
+		
 	}
 
 	public void placeQueen() // Enter queen at user's specified position
 	{
 		Scanner scan = new Scanner(System.in);
 		String boardPosition = scan.next();
+
+		while (boardPosition.length() != 2) {
+			System.out.println("Please enter a position between A1 & H8:");
+			boardPosition = scan.next();
+		}
+
+		int column = boardPosition.charAt(0) - 'a';
+		int row = boardPosition.charAt(1) - 49;
 		
-			while (boardPosition.length() != 2) {
-				System.out.println("Please enter a position between A1 & H8:");
-				boardPosition = scan.next();
+		System.out.println(column);
+		System.out.println(row);
+
+		for (int i = 0; i < chessBoard.length; i++) {
+			for (int j = 0; j < chessBoard[i].length; j++) {
+				if (i == column && j == row)
+
+					System.out.print("Q");
 			}
-	
-			int column = boardPosition.charAt(0) - 'a';
-			int row = boardPosition.charAt(0) - '1';
-			
-			
-			 for(int i = 0; i < column; i++)
-			{
-				for(int j = 0; j < row; j++)
-				{
-					if(i == column && j == row)
-						
-					System.out.print("Q");	
-			}
-			}
-			
-	    
-			
+		}
+
 		scan.close();
 	}
 }
