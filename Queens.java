b415794bd2fc852a7c2aeef8757426ba539fada8
CharlieOConnor <charrlie.oconnor98@gmail.com>
2018-11-26 14:16:36 +0000

    Update Queens.java

--- Queens.java
+++ Queens.java
@@ -10,40 +10,43 @@ public class Queens {
 
 	public Queens() {
 
-		int numPosition = 8; // Placeholder for number positions on the chess
-								// board
+		
+		
 		for (int i = 0; i < chessBoard.length; i++) {
 			for (int j = 0; j < chessBoard[i].length; j++) {
 				chessBoard[i][j] = " ";
 			}
 		}
 		
-		System.out.print("This is an 8 Queens chessboard\n\n");
-		System.out.print("Please enter a position on the board (i.e. D3)\n");
-
+				
 		placeQueen();
+		printBoard();
+	}
+    public void printBoard() 
+    {
+    	int numPosition = 1; // Placeholder for number positions on the chess board
+    	
+    	System.out.print("This is an 8 Queens chessboard\n\n");
 
 		System.out.print("\n    a   b   c   d   e   f   g   h");
 
-		for (int row = 0; row < chessBoard.length; row++) // A nested for loop
-															// to create the
-															// chess board
+		for (int row = 0; row < chessBoard.length; row++) // A nested for loop to create the chess board
 		{
-			System.out.print("\n  +-------------------------------+\n" + numPosition-- + " |");
+			System.out.print("\n  +-------------------------------+\n" + numPosition++ + " |");
 
 			for (int column = 0; column < chessBoard[row].length; column++) {
 				System.out.print(" ");
-				System.out.print(chessBoard[row][column]);
+				System.out.print(chessBoard[row][column]); //Adds space in-between pipes
 				System.out.print(" |");
 			}
 		}
 		System.out.print("\n  +-------------------------------+\n");
-		
-	}
-
+    }
+    
 	public void placeQueen() // Enter queen at user's specified position
 	{
 		Scanner scan = new Scanner(System.in);
+		System.out.print("Please enter a position on the board (i.e. D3)\n");
 		String boardPosition = scan.next();
 
 		while (boardPosition.length() != 2) {
@@ -56,15 +59,15 @@ public class Queens {
 		
 		System.out.println(column);
 		System.out.println(row);
-
-		for (int i = 0; i < chessBoard.length; i++) {
+        chessBoard[row][column] = "Q";
+        
+		/* for (int i = 0; i < chessBoard.length; i++) {
 			for (int j = 0; j < chessBoard[i].length; j++) {
 				if (i == column && j == row)
 
 					System.out.print("Q");
 			}
-		}
-
+		} */
 		scan.close();
 	}
 }
