716335352c4b26a5a3fb81b4045f16d80acf53d2
CharlieOConnor <charrlie.oconnor98@gmail.com>
2018-11-28 14:21:34 +0000

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,11 +4,14 @@ public class Main {
 
 	public static void main(String args[]) {
 		
+		Queens Queens = new Queens(); //Create an instance of the Queens class
+		
 		Queens.setup();
 		Queens.placeFirstQueen();
 		Queens.markConflicts();
 		//Queens.printBoard();
 		Queens.placeRemainingQueens();
+		Queens.removeDots(); 
 		Queens.printBoard();
 	}
 }
